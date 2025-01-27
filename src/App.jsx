163c4979af261f3a5857a38ab899b08d3ee29090
Renan Nascimento <atomsfrontend@gmail.com>
2025-01-27 19:37:00 -0300

    feat: created handle render routes client side

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Templates
+import Index from "./Templates/index.jsx";
+import Home from "./Templates/home.jsx";
+
+// Components
+import UserAccount from './components/user-accounts.tsx'
+import Dashboard from './components/dashboard.tsx'
+import Records from './components/records.tsx'
+import Settings from './components/settings.tsx'
+
+export const Error = () => {
+  return (
+    <div class="container">
+      <h1>Error</h1>
+    </div>
+  );
+}
+
+export const PrivateRoute = ({children}) => {
+  const token = localStorage.getItem('token');
+  if(!token) {
+    return (
+      <Navigate to={"/"}/>
+    )
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {children}
     </>
   )
 }
 
-export default App
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/home" element={
+            <PrivateRoute>
+              < Home/>
+            </PrivateRoute>
+          } 
+        > 
+          {/* Children Routes Home */}
+          <Route index element={<Dashboard />} /> {/* Default Route */}
+          <Route path="records" element={<Records />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+         <Route path="/user/account" element={
+          <PrivateRoute>
+            <UserAccount/>
+          </PrivateRoute>
+        } />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
