d48bbd4671927100b8d74553db0dce98218677aa
Renan Nascimento <atomsfrontend@gmail.com>
2025-01-27 19:38:33 -0300

    feat: created outlet to render sub-routes inside home

--- src/Templates/home.jsx
+++ src/Templates/home.jsx
@@ -1,16 +1,16 @@
 // Hooks
-import { useNavigate } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 
 // Components
 import { AppSidebar } from "../components/app-sidebar"
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
+// import {
+//   Breadcrumb,
+//   BreadcrumbItem,
+//   BreadcrumbLink,
+//   BreadcrumbList,
+//   BreadcrumbPage,
+//   BreadcrumbSeparator,
+// } from "@/components/ui/breadcrumb"
 import { Separator } from "@/components/ui/separator"
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 
@@ -30,27 +30,20 @@ export default function Page() {
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
+            {/* <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
+                  <BreadcrumbLink>Home</BreadcrumbLink>
                 </BreadcrumbItem>
                 <BreadcrumbSeparator className="hidden md:block" />
                 <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                  <BreadcrumbPage>Dashboard</BreadcrumbPage>
                 </BreadcrumbItem>
               </BreadcrumbList>
-            </Breadcrumb>
+            </Breadcrumb> */}
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0 bg-slate-200">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3 bg-slate-300">
-            <div className="aspect-video rounded-xl bg-muted/50 bg-slate-400" />
-            <div className="aspect-video rounded-xl bg-muted/50 bg-slate-500" />
-            <div className="aspect-video rounded-xl bg-muted/50 bg-slate-600" />
-          </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min bg-slate bg-slate-700" />
-        </div>
+        <Outlet />
       </SidebarInset>
     </SidebarProvider>
   )
